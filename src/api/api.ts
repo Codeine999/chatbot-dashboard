78b242abdf2fff58ff8f888db951af05c285e62b
petunda <petunda@ecgoldtech.com>
2026-06-21 20:54:11 +0700

    env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
