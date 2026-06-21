33634b8e9d1a462195ffce5e0f04de4fdcf2bb8a
petunda <petunda@ecgoldtech.com>
2026-06-21 21:02:43 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://4b9b-2405-9800-b640-e82b-34e9-be08-f59c-dbd1.ngrok-free.app",
+        target: "https://c9c1-2405-9800-b640-e82b-34e9-be08-f59c-dbd1.ngrok-free.app",
         changeOrigin: true,
         headers: {
+          "Content-Type": "application/json",
           "ngrok-skip-browser-warning": "true",
         },
       },
